663d46a5f86cb78c3dc97c0f4eb58319881edb98
therealgatabazi <d.gatabazi@alustudent.com>
2023-03-10 03:15:22 +0200

    file 0 tryout

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,15 +5,14 @@ import requests
 
 def subscribers_count(subreddit_name):
     """DOC"""
-    subreddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit_name)
+    subreddit_url = f"https://www.reddit.com/r/{subreddit_name}/about.json"
 
     user_agent = {'User-agent': 'Mozilla/5.0'}
-    subreddit_response = requests.get(subreddit_url,
-                                      headers=user_agent)
+    subreddit_response = requests.get(subreddit_url, headers=user_agent)
 
     if subreddit_response.status_code == 200:
         subreddit_data = subreddit_response.json()['data']
         subscriber_count = subreddit_data['subscribers']
         return subscriber_count
-    return 0
+    else:
+        return 0
