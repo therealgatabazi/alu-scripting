14aa169d0718c19feb06c0abb036f783246d371f
therealgatabazi <d.gatabazi@alustudent.com>
2023-03-10 03:00:44 +0200

    changed code 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,9 +4,10 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the top ten posts from a given subreddit."""
-    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
